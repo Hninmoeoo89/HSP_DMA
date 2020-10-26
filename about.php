bcc6fd676a53cc03b710ceb72513aab92eb37656
Hnin <hninmoeoo24@gmail.com>
2020-10-26 09:42:13 +0630

    f

---
+++ about.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">  
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>About</title>
+  <link rel="stylesheet" href="library/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="library/fontawesome/fontawesome-all.min.css">
+  
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="shortcut icon" href="images/Logo-Website/32x32 px - png-logo.png">
+</head>
+<style>
+        .text-red{ 
+                  color: #8D4242;
+                }
+          h4 {
+          width: 250px;
+          height: 60px;
+          line-height: 60px;
+          text-align: center;
+          border-radius: 20px 20px 20px 20px;
+         }    
+         h4.bg-brown{
+          background-color: #8D4242;
+         }    
+         h5.logo {outline: thick ridge pink;
+            padding: 3px;
+            width: 500px;
+            color: #8D4242;}
+        ul {
+             list-style: none;
+          }
+        ul li::before {
+            content: "\2022";
+            color: #8D4242;
+            font-weight: bold;
+            display: inline-block; 
+            width: 2em;
+            margin-left: -1em;
+              }
+              .text-red{ 
+      color: #8D4242;
+    } 
+   
+    </style>
+<body>
+
+    <section class="bg-light">
+     <div class="container by-3">
+      <div class="row text-muted">
+        <div class="col-3 text-center">
+          <div class="input-group my-3">
+                    <button class="input-group-addon btn btn-info" id="sizing-addon2">ရှာဖွေရန်</button>
+
+                       <input type="text" class="form-control" aria-describedby="sizing-addon2">
+                </div>
+        </div>
+        <div class="social-list col-9 text-right my-3">
+             <!-- <a href="#"><img src="images/12.jpg"></a>
+             <a href="#"><img src="images/united-kingdom.png" class="ml-auto mr-3"></a> -->
+        </div>
+
+      </div>
+     </div>
+    </section>
+
+
+
+
+        <div class="container">
+          <div class="row mt-5">
+            <div class="col-md-4">
+              <a href="index.php">
+              <img src="images/Logo-Website/Original Logo-png.png" alt="" width="100px;" class="mr-auto mb-2">
+              </a>
+            </div>
+            <div class="col-md-8 mr-auto"> 
+            <h4 class="text-center text-white bg-brown">
+              ကျွန်ုပ်တို့အကြောင်း
+              </h4>             
+            </div>
+          </div>
+        </div>
+        
+  <section id="#about" class="my-5 px-3">
+    <div class="container">         
+          <div class="row my-3 mx-0" style="background-color: #cce6ff;">
+            <div class="col-sm-5 pl-0 mt-3">
+              <img src="images/Clients/HSP/About - HSP/slide.png" alt="" class="img-fluid ml-5" width="400px;" height="200px;">
+            </div>
+            <div class="col-sm-7 mt-5" >
+              <h5 class="text-red"><b>Facebook Marketing</b></h5>
+              <h6 class="text-red py-4">
+                ကျွန်မတို့၏ ဆန်းသစ်သော Idea များ၊ Digital Marketing နှင့် ပတ်သက်၍ ရရှိထားသော အတွေ့အကြုံများဖြင့် လူကြီးမင်းတို့၏ Product တွေ၊ Brand တွေကို Customer များ ဝယ်ယူအသုံးပြုလာရန် ဖန်တီးပေးပြီး ပုံမှန် Facebook Follower များမှတစ်ဆင့် လူကြီးမင်းတို့၏ Product များကို အမှန်တကယ်သုံးစွဲသော Customer များဖြစ်လာစေရန်ကူညီဆောင်ရွက်ပေးနေပါသည်။ 
+              </h6>
+            </div>
+          </div>
+    </div>
+  </section>
+  <section id="#about" class="my-5 px-3">
+    <div class="container">
+          <div class="row my-3 mx-0" style="background-color: #B6F474;">
+            <div class="col-sm-5 pl-0 mt-3">
+              <img src="images/Clients/HSP/About - HSP/display.jpg" alt="" class="img-fluid ml-5" width="400px;" height="200px;">
+            </div>
+            <div class="col-sm-7 mt-5">
+              <h5 class="text-red"><b>Messenger Marketing</b></h5> 
+              <h6 class="text-red py-4">
+                လူကြီးမင်းတို့ Page ရဲ့ Messenger မှာ Chatbot တည်ဆောက်ခြင်းဖြင့် လူကြီးမင်းတို့၏ လုပ်ငန်း အလျင်အမြန်တိုးတက်စေရန်အတွက် Message Box မှ အလိုအလျောက်ဖြေကြားမှုများ၊ Customer မေးသော အခြားသော မေးခွန်းများအား ဖြေဆိုပေးခြင်း၊ လုပ်ငန်းအမျိုးအစားပေါ်မူတည်ပြီး Chatbot တည်ဆောက်ပေးခြင်းတို့ ပြုလုပ်ပေးသွားမှာ ဖြစ်ပါတယ်....
+              </h6>
+            </div>
+          </div>
+    </div>
+  </section>
+
+
+  <div class="container">
+    <div class="row">
+      <h5 class="logo my-5 text-center">Logo Marketing</h35>      
+    </div>
+    <div class="row mt-5">
+      <div class="col-md-4">
+        <img src="images/Clients/HSP/Post-32-Logo.png" alt="" class="img-fluid">
+      
+        </div>
+        <div class="col-md-8">
+          <div class="row mt-5">
+          <div class="col-md-1">
+            <img src="images/Clients/HSP/About - HSP/Step-1.png" alt="" width="50px;">
+          </div>
+          <div class="col-md-10">
+            <h5 class="text-red">ကျွန်မတို့ရဲ့ HSP Digital Marketing Angency မှ Graphic Designers များက အမိုက်စား Logo တွေကို သူမတူတဲ့ Design ရေးဆွဲပေးနေပါတယ်။</h5>
+          </div>
+            </div>
+            <div class="row mt-5">
+          <div class="col-md-1">
+            <img src="images/Clients/HSP/About - HSP/Step-2.png" alt="" width="50px;">
+          </div>
+          <div class="col-md-10">
+             <h5 class="text-red">Customer များ စိတ်ကြိုက်ဖြစ်စေမယ့် Logo Design ရေးဆွဲပေးနေပါတယ်။</h5>
+          </div>
+            </div>
+          
+                   <!--  <h5>ကျွန်မတို့ရဲ့ HSP Digital Marketing Angency မှ Graphic Designers များက အမိုက်စား Logo တွေကို သူမတူတဲ့ Design တွေနဲ့ ရေးဆွဲနေပါတယ်။</h5>
+                   <h5>Customer များ စိတ်ကြိုက်ဖြစ်စေမယ့် Logo Design များကို ရေးဆွဲပေးပါတယ်။</h5> -->
+                
+        </div>
+    </div>
+    
+  </div>
+
+  
+
+  <!-- contact section start -->
+    <footer class="footer bg-light mt-5">
+      <div class="container-fluid py-3 mb-5">
+        <div class="row">
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">Follow Us  : </h3>
+             <a href="https://www.facebook.com/hspdigitalmarketingagency/about/?ref=page_internal"><img src="images/fb.PNG" width="250px;" height="350px;"></a>
+          </div>
+
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">ဆက်သွယ်ရန်နေရာ   :</h3>
+             <a href="https://www.google.com/maps/d/u/0/viewer?msa=0&mid=1KknUe0qoKPJWFL-hpWMm4CYLEFg&ll=18.821413334887794%2C95.2319207725624&z=16">
+               <img src="images/map2.PNG" width="325px;">
+             </a>
+          </div>
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">ဆက်သွယ်ရန်လိပ်စာ :</h3>
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>လိပ်စာ :</b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">No.157,Bogyoke Road,Ywar Bae East Ward,Near CB Bank(2),Pyay,Bago Region,Myanmar.08156</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>ဖုန်းနံပါတ် : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">09 893 19994</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>အီးမေးလ် : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">hsp.marketingagency@gmail.com</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>Customer Service : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">Facebook Marketing Service, Messager Marketing Service, Logo Design Service</p>
+                </div>
+              </div>
+
+
+          </div>
+          </div>
+
+        <div class="copyright text-center">
+          copyright &copy; 2020 | <span class="text-primary"> HSP Digital Marketing Agency</span>
+        </div>
+        </div>
+      </div>
+    </footer>
+
+  <!-- contact section end -->
+
+
+
+
+
+
+
+    <script src="library/bootstrap/jquery-3.4.1.slim.min.js"></script>
+    <script src="library/bootstrap/popper.min.js"></script>
+    <script src="library/bootstrap/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>

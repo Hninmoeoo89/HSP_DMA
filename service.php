ef6f4e5753cb7412dfd0f043c0f5d063cdb3e209
Hnin <hninmoeoo24@gmail.com>
2020-10-26 09:46:59 +0630

    nine

---
+++ service.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>HSP Digital Marketing Agency</title>
+	<link rel="stylesheet" href="library/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="library/fontawesome/fontawesome-all.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="short icon" href="images/Logo-Website/32x32 px - Logo.jpg">
+	<style>
+		.card{
+			padding: 0;
+		}
+		.btn{
+			border-radius: 0;
+		}
+		.btn-red{
+			background-color: #A14040;
+		}
+		h2.bg-brown{
+			background-color: #8D4242;
+		}
+	</style>
+</head>
+<body>
+	
+<section id="service" class="bg-light">
+	<div class="container mt-3">
+		<div class="row">
+			<div class="col-md-3">
+				<a href="index.php">
+			<img src="images/Logo-Website/Original Logo-png.png" alt="" width="100px;" class="mr-auto mb-2">
+		    </a>
+			</div>
+		<div class="col-md-7 ml-auto">
+		<h2 class="text-center bg-brown text-white">ဝန်ဆောင်မှုများ</h2>
+		</div>
+		</div>
+	</div>
+	<div class="container mt-2">
+			<div class="row">
+				<div class="col-md-6 mt-3">
+					<div class="card">
+						<div class="row ml-3 mt-2">
+							<img src="images/Clients/HSP/About - HSP/About-FM.png" alt="" width="500px;">
+								
+						</div>
+						<div class="row ml-3 mt-3">
+							<img src="images/Clients/HSP/About - HSP/Features.png" alt="" width="150px;">
+						</div>
+						<div class="row mt-3 ml-3 mb-5">
+							<img src="images/Clients/HSP/About - HSP/Features-FM.png" alt="" width="400px;">
+						</div>
+						<a href="fa-advantages.php" class="btn btn-red"><h4 class="text-white">Advantages</h4></a>
+						
+					</div>
+				</div>
+				<div class="col-md-6 mt-3">
+					<div class="card">
+					<div class="row ml-3">
+						<img src="images/Clients/HSP/About - HSP/About-MM.png" alt="" width="500px;">
+							
+					</div>
+					<div class="row mt-2 ml-3">
+						<img src="images/Clients/HSP/About - HSP/Features.png" alt="" width="150px;">
+					</div>
+					<div class="row ml-1">
+						<img src="images/Clients/HSP/About - HSP/Features-MM1.png" alt="" width="545px;">
+					</div>
+					<a href="messanger-advantages.php" class="btn btn-red"><h4 class="text-white">Advantages</h4></a>
+					
+					
+					</div>
+				</div>
+			</div>
+	</div>
+</section>
+
+
+
+  <!-- contact section start -->
+    <footer class="footer bg-light mt-5">
+      <div class="container-fluid py-3 mb-5">
+        <div class="row">
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">Follow Us  : </h3>
+             <a href="https://www.facebook.com/hspdigitalmarketingagency/about/?ref=page_internal"><img src="images/fb.PNG" width="250px;" height="350px;"></a>
+          </div>
+
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">ဆက်သွယ်ရန်နေရာ   :</h3>
+             <a href="https://www.google.com/maps/d/u/0/viewer?msa=0&mid=1KknUe0qoKPJWFL-hpWMm4CYLEFg&ll=18.821413334887794%2C95.2319207725624&z=16">
+               <img src="images/map2.PNG" width="325px;">
+             </a>
+          </div>
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">ဆက်သွယ်ရန်လိပ်စာ :</h3>
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>လိပ်စာ :</b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">No.157,Bogyoke Road,Ywar Bae East Ward,Near CB Bank(2),Pyay,Bago Region,Myanmar.08156</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>ဖုန်းနံပါတ် : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">09 893 19994</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>အီးမေးလ် : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">hsp.marketingagency@gmail.com</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>Customer Service : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">Facebook Marketing Service, Messager Marketing Service, Logo Design Service</p>
+                </div>
+              </div>
+
+
+          </div>
+          </div>
+
+        <div class="copyright text-center">
+          copyright &copy; 2020 | <span class="text-primary"> HSP Digital Marketing Agency</span>
+        </div>
+        </div>
+      </div>
+    </footer>
+
+  <!-- contact section end -->
+
+<script src="library/bootstrap/jquery-3.4.1.slim.min.js"></script>
+<script src="library/bootstrap/popper.min.js"></script>
+<script src="library/bootstrap/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>

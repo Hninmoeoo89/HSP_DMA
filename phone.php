52a50c434c9a7cff4d0924c7b2166764471cdfe7
Hnin <hninmoeoo24@gmail.com>
2020-10-26 09:46:28 +0630

    eight

---
+++ phone.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>HSP Digital Marketing Agency</title>
+	<link rel="stylesheet" href="library/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="library/fontawesome/fontawesome-all.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="short icon" href="images/Logo-Website/32x32 px - Logo.jpg">
+</head>
+<body>
+	<section id="phone">
+		<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+			<a href="index.php">
+			<img src="images/Logo-Website/Original Logo-png.png" alt="" width="100px;" class="mr-auto mb-2">
+		    </a>
+		    </div>
+		    <div class="col-md-8 py-5">
+		    	<video controls="" width="500px;">
+		    		<source src="images/Clients/Power Mobile/Post-3-Chatbot အသုံးပြုပုံ.mp4" type="video/mp4">
+		    	</video>
+		    </div>
+		</div>
+		</div>
+		<div class="container my-5">
+		<h1 class="text-center my-3 bg-red text-white mr-auto">မိုဘိုင်းဖုန်းများ</h1>
+		
+			<div class="row">
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post 1 - Welcome Post.jpg" alt="" width="250px;">
+				</div>
+
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post 2 - Auto Reply Chatbot.jpg" alt="" width="250px;">
+				</div>
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post-4-Knowledge Battery.jpg" alt="" width="250px;">
+				</div>
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post-5-Request for messenger.jpg" alt="" width="250px;">
+				</div>
+			</div>
+			<div class="row mt-3">
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post-7-Huawei Y6P.jpg" alt="" width="250px;">
+				</div>
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post-7-Huawei Y6P-1.jpg" alt="" width="250px;">
+					
+				</div>
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post-9-Mi K30-1.jpg" alt="" width="250px;">
+					
+				</div>
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post-9-Mi K30-2.jpg" alt="" width="250px;">
+					
+				</div>
+			</div>
+
+			<div class="row mt-3">
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post-10-Mi K30 Pro.jpg" alt="" width="250px;">
+					
+				</div>
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post-10-Mi K30 Pro-1.jpg" alt="" width="250px;">
+					
+				</div>
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post-8-Huawei Nova 7SE.jpg" alt="" width="250px;">
+				</div>
+				<div class="col-md-3">
+					<img src="images/Clients/Power Mobile/Post-10-Mi K30 Pro.jpg" alt="" width="250px;">
+				</div>
+			</div>
+		</div>
+    </section>
+
+
+
+  <!-- contact section start -->
+    <footer class="footer bg-light">
+      <div class="container-fluid py-3 mb-5">
+        <div class="row">
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">Follow Us  : </h3>
+             <a href="https://www.facebook.com/hspdigitalmarketingagency/about/?ref=page_internal"><img src="images/fb.PNG" width="250px;" height="350px;"></a>
+          </div>
+
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">ဆက်သွယ်ရန်နေရာ   :</h3>
+             <a href="https://www.google.com/maps/d/u/0/viewer?msa=0&mid=1KknUe0qoKPJWFL-hpWMm4CYLEFg&ll=18.821413334887794%2C95.2319207725624&z=16">
+               <img src="images/map2.PNG" width="325px;">
+             </a>
+          </div>
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">ဆက်သွယ်ရန်လိပ်စာ :</h3>
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>လိပ်စာ :</b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">No.157,Bogyoke Road,Ywar Bae East Ward,Near CB Bank(2),Pyay,Bago Region,Myanmar.08156</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>ဖုန်းနံပါတ် : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">09 893 19994</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>အီးမေးလ် : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">hsp.marketingagency@gmail.com</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>Customer Service : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">Facebook Marketing Service, Messager Marketing Service, Logo Design Service</p>
+                </div>
+              </div>
+
+
+          </div>
+          </div>
+
+        <div class="copyright text-center">
+          copyright &copy; 2020 | <span class="text-primary"> HSP Digital Marketing Agency</span>
+        </div>
+        </div>
+      </div>
+    </footer>
+
+  <!-- contact section end -->
+
+		
+
+
+
+<script src="library/bootstrap/jquery-3.4.1.slim.min.js"></script>
+	<script src="library/bootstrap/popper.min.js"></script>
+	<script src="library/bootstrap/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
